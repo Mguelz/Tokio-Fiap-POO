58f6edf32cc4ae346347393d9a187a5d12460ada
Miguel <945507756m@gmail.com>
2023-06-26 22:40:00 -0300

    exercicio 2 - apostila 6

--- src/Aulas/a07/Usuario.java
+++ src/Aulas/a07/Usuario.java
@@ -1,10 +1,13 @@
 package Aulas.a07;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
- * 
+ * EXERCICIO 1
  * 1. Crie uma classe Usuario 
  * 2. Adicione os atributos idUsuario, nome e senha
  * 3. Crie um construtor 
@@ -26,17 +29,49 @@ import java.util.List;
  * } return 0;
  * 
  * } 
+ * 
  * 8. Crie uma classe de teste com método main 
  * 9. Crie 5 usuários e adicione a uma lista de usuários 
  * 10. Utilize Collections.sort para ordenar os usuários da lista; Collections.sort(usuarios) 
  * 11. Faça um laço com foreach para imprimir o id dos usuários já ordenados.
  * 
- * 
+ * EXERCICIO 2
+ * • Implemente chaves para os usuários da classe Usuarios.
+ * • Crie um método para recuperar os objetos pelas chaves e imprima uma lista de nome de usuários.
  * 
  * @author Miguel
  *
  */
 public class Usuario implements Comparable<Usuario> {
+
+	public static void main(String[] args) {
+		// exercicio 1
+		System.out.println("Exercicio 1");
+		List<Usuario> listaUsuario = new ArrayList<Usuario>();
+
+		listaUsuario.add(new Usuario(1, "miguel", "miguel123"));
+		listaUsuario.add(new Usuario(4, "jose", "jose123"));
+		listaUsuario.add(new Usuario(2, "marta", "marta123"));
+		listaUsuario.add(new Usuario(5, "joaquim", "joaquim123"));
+		listaUsuario.add(new Usuario(3, "edson", "edson123"));
+
+		ordenarPorId(listaUsuario);
+		imprimirListaUsuario(listaUsuario);
+
+		// exercicio 2
+		System.out.println("\nExercicio 2");
+		Map<String, Usuario> mapUsuario = new HashMap<>(); // criando um map de Usuario onde k = String / v = Usuario
+		mapUsuario.put("miguel", listaUsuario.get(0));
+		mapUsuario.put("marta", listaUsuario.get(2));
+		mapUsuario.put("edson", listaUsuario.get(4));
+		mapUsuario.put("jose", listaUsuario.get(1));
+		mapUsuario.put("joaquim", listaUsuario.get(3));
+		imprimirMapUsuario(mapUsuario);
+		// ---------------------------------------------------------------------------
+		System.out.println("\nRecuperando a senha de acordo com o nome dado: ");
+		recuperarSenhaUsuario(mapUsuario, "miguel");
+	}
+
 	private int idUsuario;
 	private String nome, senha;
 
@@ -44,14 +79,17 @@ public class Usuario implements Comparable<Usuario> {
 		this.idUsuario = idusuario;
 		this.nome = nome;
 		this.senha = senha;
+
+//		mapUsuario.put(nome, this);
+
 	}
 
 	public int getIdUsuario() {
 		return idUsuario;
 	}
 
-	public void setIdUsuario(int idusuario) {
-		this.idUsuario = idusuario;
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
 	}
 
 	public String getNome() {
@@ -94,6 +132,22 @@ public class Usuario implements Comparable<Usuario> {
 	public static void imprimirListaUsuario(List<Usuario> listaUsuario) {
 		for (Usuario id : listaUsuario) {
 			System.out.println(id);
+		}
+	}
+
+	public static void imprimirMapUsuario(Map<String, Usuario> map) {
+		System.out.println("Lista de nomes de usuarios:");
+		for (String nomeUsuario : map.keySet()) {
+			System.out.println(nomeUsuario);
+		}
+	}
+
+	public static void recuperarSenhaUsuario(Map<String, Usuario> map, String nome) {
+		Usuario usuario = map.get(nome);
+		if (usuario != null) {
+			System.out.println("senha recuperada de " + nome + ": " + usuario.getSenha());
+		} else {
+			System.out.println("Usuario nao " + nome + " encontrado!");
 		}
 	}
 
