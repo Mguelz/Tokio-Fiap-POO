baf34baad992d8e7a9aa249466bd7fccee76d913
logonrmlocal <logonrmlocal@PA603MICRO02.fiap.com.br>
2023-06-28 14:13:37 -0300

    correcao exercicio de Map - 28/06/2023

--- src/Aulas/a07/UsuarioProfessor.java
+++ src/Aulas/a07/UsuarioProfessor.java
@@ -5,12 +5,13 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 /**
  * 
  * 
  * 
  * 
- * PEGAR ESSA CLASSE QUE ALGUEM FEZ                                                                                                                                    (PQP)
+ * PEGAR ESSA CLASSE QUE ALGUEM FEZ (PQP)
  * 
  * 
  * 
@@ -18,57 +19,63 @@ import java.util.Map;
  *
  */
 public class UsuarioProfessor implements Comparable<Usuario> {
-	static Map<Object, Object> mapa = new HashMap<Object, Object>();//Não é declarado nenhum tipo pois o map pode guardar diversos tipos.
+	static Map<Object, Object> mapa = new HashMap<Object, Object>();// Não é declarado nenhum tipo pois o map pode
+																	// guardar diversos tipos.
+
 	public static void main(String[] args) {
+		System.out.println("---------- Lista Desordenada -------------");
+
 		Usuario guilherme = new Usuario(3, "Guilherme R", "123321");
 		Usuario emerson = new Usuario(2, "Emerson", "abc");
 		Usuario miguel = new Usuario(1, "Miguel", "123456");
-		List<Usuario> users = new ArrayList<Usuario>();
-		users.add(guilherme);
-		users.add(emerson);
-		users.add(miguel);
-		
-		
-		//Iterando a lista.
-		for (Usuario user : users) {
-			System.out.println(user.getNome());
-		}
-		
-		
-		
-		//Reordena a lista conforme a regra definida no CompareTo.
-		Collections.sort(users);
-		
-		//Iterando a lista ordenada.
-		for (Usuario user : users) {
-			System.out.println(user.getNome());
+		List<Usuario> listaUsuario = new ArrayList<Usuario>();
+		listaUsuario.add(guilherme);
+		listaUsuario.add(emerson);
+		listaUsuario.add(miguel);
+
+		// Iterando a lista.
+		for (Usuario user : listaUsuario) {
+			System.out.println("Id: " + user.getIdUsuario() + " -- Nome: " + user.getNome());
 		}
 
+		// Reordena a lista conforme a regra definida no CompareTo.
+		Collections.sort(listaUsuario);
+
+		System.out.println("\n---------- Lista Ordenada ----------------");
+
+		// Iterando a lista ordenada.
+		for (Usuario usuario : listaUsuario) {
+			System.out.println("Id: " + usuario.getIdUsuario() + " -- Nome: " + usuario.getNome());
+		}
 		
-		mapa.put(guilherme.getIdUsuario(), guilherme);
-		mapa.put(miguel.getIdUsuario(),miguel);
-		mapa.put(emerson.getIdUsuario(),emerson);
-		
-		//Recuperando o objeto por meio da chave:
-//		guilherme.recuperaUsuario();
-		
+		System.out.println("\n---------- Usuario Recuperado ----------------");
+
+
+		Map<String, Usuario> map = new HashMap<String, Usuario>();
+
+		map.put("miguel", miguel);
+
+		Object objeto = map.get("miguel");
+		Usuario usuario = (Usuario) objeto;
+		System.out.println("Usuario - " + usuario.getNome() + " id - " + usuario.getIdUsuario());
+
 	}
 
-	private long id;
+	private long idUsuario;
 	private String nome, senha;
 
 	public UsuarioProfessor(long id, String nome, String senha) {
-		this.id = id;
+		this.idUsuario = id;
 		this.nome = nome;
 		this.senha = senha;
 	}
 
 	public long getId() {
-		return id;
+		return idUsuario;
 	}
 
 	public void setId(long id) {
-		this.id = id;
+		this.idUsuario = id;
 	}
 
 	public String getNome() {
